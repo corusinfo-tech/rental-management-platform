bad55678e1663011bcc29fb8ab619e0c4c6444fd
corusinfo-tech <dev.corusinfo@gmail.com>
2026-07-14 14:50:41 +0530

    Add files via upload

---
+++ 1.eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    es2022: true,
+    node: true,
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    '.next/',
+    'coverage/',
+    '.turbo/',
+  ],
+};
